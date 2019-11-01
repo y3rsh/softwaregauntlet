9d98b18f244df459388389bd74c102913c2c4f07
Stockton <cstockto@teksystems.com>
2019-11-01 16:47:57 -0700

    Add 'Source File' repository

--- src/main/java/com/ally/cia/ingestion/sourcefile/SourceFileProvider.java
+++ src/main/java/com/ally/cia/ingestion/sourcefile/SourceFileProvider.java
@@ -1,11 +1,6 @@
 package com.ally.cia.ingestion.sourcefile;
 
-import com.softwareonpurpose.gauntlet.Environment;
-
-import java.io.File;
-
 public class SourceFileProvider {
-    private static final String INGESTION_FILE_PATH = Environment.getInstance().getIngestionFilePath();
     private static SourceFileProvider provider;
 
     public static SourceFileProvider getInstance() {
@@ -16,7 +11,6 @@ public class SourceFileProvider {
     }
 
     public SourceFile get(int jobId) {
-        final String filePath = String.format("%s/jobNumber%d.txt", INGESTION_FILE_PATH, jobId);
-        return SourceFile.getInstance(new File(filePath));
+        return SourceFileRepository.getInstance().query(jobId);
     }
 }
